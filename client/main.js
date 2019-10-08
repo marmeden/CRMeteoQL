9986b519830b6373f2a31e4747029e0a03e96de8
marmeden <itsme@eneasmarin.es>
2019-10-08 10:44:50 +0200

    todos

--- client/main.js
+++ client/main.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
 
+import '../imports/startup/accounts-config.js';
 import App from '../imports/ui/App.js';
-
+ 
 Meteor.startup(() => {
-  ReactDOM.render(<App />, document.getElementById('app'));
+  render(<App />, document.getElementById('render-target'));
 });
